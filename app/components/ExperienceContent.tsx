cbca4fd06edfb13c55be3a61e2db93afb64d04c1
FrostGreg <gregfrost305@gmail.com>
2025-01-29 22:06:40 +0000

    update current experience

--- app/components/ExperienceContent.tsx
+++ app/components/ExperienceContent.tsx
@@ -51,8 +51,15 @@ const ExperienceArticle = ({
   </motion.article>
 );
 
+const isBeforeOctober2025 = () => {
+  const date = new Date();
+  return date.getFullYear() == 2025 && date.getMonth() <= 9;
+};
+
 const ExperienceContent = () => {
   const [tabNumber, setTabNumber] = useState(0);
+  const [showPresent] = useState(isBeforeOctober2025());
+  const [servicesDuration] = useState(Math.min(new Date().getMonth() + 3, 12));
   return (
     <motion.div
       initial={{ y: 1000 }}
@@ -95,21 +102,39 @@ const ExperienceContent = () => {
         </li>
       </ul>
       {tabNumber === 0 && (
-        <ExperienceArticle
-          title="Web Developer"
-          date="June 2022 - Present"
-          employer={{
-            name: "Capital One",
-            link: "https://www.capitalone.co.uk/",
-          }}
-          bulletOne="Develop simple, easy-to-maintain, and performant code for various
+        <>
+          <ExperienceArticle
+            title="Back-end Services Engineer"
+            date={`September 2024 - ${
+              showPresent ? "Present" : "October 2025"
+            } ~${servicesDuration} months`}
+            employer={{
+              name: "Capital One",
+              link: "https://www.capitalone.co.uk/",
+            }}
+            bulletOne="Setup accurate, reliable, and secure logging dashboards and alerts to allow faster troubleshooting"
+            bulletTwo="Extend functionality and develop new microservices adhering to strict quality and performance metrics"
+            bulletThree="Deploy, maintain, and decommission various AWS infrastructure"
+          />
+          <ExperienceArticle
+            title="Mobile Tester"
+            date="April 2023 - September 2023 ~6 months"
+            bulletOne="Facilitate execution of End-to-End testing across new mobile features ensuring quality"
+            bulletTwo="Build upon and maintain a resilient test suite including unit, functional acceptance, accessibility, and jira x-ray tests"
+            bulletThree="Ensure an equal experience across both iOS and Android platforms"
+          />
+          <ExperienceArticle
+            title="Web Developer"
+            date="June 2022 - April 2023 ~9 months"
+            bulletOne="Develop simple, easy-to-maintain, and performant code for various
                web applications"
-          bulletTwo="Work with a variety of different languages and frameworks such as
+            bulletTwo="Work with a variety of different languages and frameworks such as
                JavaScript, TypeScript, React, Node.js, and Cypress"
-          bulletThree="Effectively convey my ideas and work collaboratively with
+            bulletThree="Effectively convey my ideas and work collaboratively with
                cross-functional teams including engineers, designers, and product
                owners to achieve project goals"
-        />
+          />
+        </>
       )}
       {tabNumber === 1 && (
         <ExperienceArticle
